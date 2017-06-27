dc8a88675c18d61abf5e4b5e7fcb15b5f90c77c1
dhaeb <dan.haeberlein@googlemail.com>
2017-06-27 14:24:49 +0200

    local merge

--- Website/js/main.js
+++ Website/js/main.js
@@ -1,6 +1,6 @@
-function loadJsonFromFS(filename){
-	var json = $.getJSON(filename, function(data){
+function loadJsonFromPath(path){
+	$.getJSON(path, function(data){ // VORSICHT, dieser Callback Handler wird nur ausgeführt, wenn das abgerufene JSON WOHLGEFORMT ist
 			console.log(data);
+			// hier können nur platzhalter im HTML (Tags mit IDs etc.) mit Leben gefüllt werden
 	});
 }
-	
