7fa65f6fba09f5bf210ff467920cd1c9dd858d79
MahitiMahitiMahiti <ger802@rebekka-guenther.de>
2017-06-28 17:46:34 +0200

    javascript ist doof und macht nicht was es soll

--- Website/js/main.js
+++ Website/js/main.js
@@ -1,6 +1,9 @@
 function loadJsonFromPath(path){
 	$.getJSON(path, function(data){ // VORSICHT, dieser Callback Handler wird nur ausgeführt, wenn das abgerufene JSON WOHLGEFORMT ist
 			console.log(data);
+			   var rolle = data.results[0].rolle;
+			
+			    $('#rolle').text(rolle);
 			// hier können nur platzhalter im HTML (Tags mit IDs etc.) mit Leben gefüllt werden
 	});
 }
