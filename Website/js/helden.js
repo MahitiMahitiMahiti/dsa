fa68b690255fe5b0888aaf612b75f956ea6feeae
MahitiMahitiMahiti <ger802@rebekka-guenther.de>
2017-06-29 12:34:52 +0200

    json für helden läuft halbwegs, json für spieler läuft komplett

--- Website/js/helden.js
+++ Website/js/helden.js
@@ -1,19 +1,18 @@
 function loadJsonFromPath(path){
 	$.getJSON(path, function(data){ // VORSICHT, dieser Callback Handler wird nur ausgeführt, wenn das abgerufene JSON WOHLGEFORMT ist
 			console.log(data);
-			   
 			   var kurzbeschreibung = data.kurzbeschreibung;
 				var aussehen = data.aussehen.kurzbeschreibung;
-				var spieltseit = data.spieltseit;
-				var regelsysteme = data.regelsysteme;
-				var genre = data.genre;
-				var helden = data.helden;				
+				var charakter = data.charakter;
+				var hintergrundgeschichte = data.hintergrundgeschichte;
+				var verbindungen = data.verbindungen;
+				var feinde = data.feinde;				
 			    $('#kurzbeschreibung').text(kurzbeschreibung);
-				$('#spielstil').text(spielstil);
-				$('#spieltseit').text(spieltseit);
-				$('#regelsysteme').text(regelsysteme);
-				$('#genre').text(genre);
-				$('#helden').text(helden);
+				$('#aussehen').text(aussehen);
+				$('#charakter').text(charakter);
+				$('#hintergrund').text(hintergrundgeschichte);
+				$('#verbindungen').text(verbindungen);
+				$('#feinde').text(feinde);
 				// hier können nur platzhalter im HTML (Tags mit IDs etc.) mit Leben gefüllt werden
 	});
 }
