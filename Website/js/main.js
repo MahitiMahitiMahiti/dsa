f656a944abdb48509cf5c17502a33a174b588f3a
Unknown <isamei12@t-online.de>
2017-07-02 20:58:34 +0200

    css Änderungen

--- Website/js/main.js
+++ Website/js/main.js
@@ -1,13 +1,13 @@
 function loadJsonFromPath(path){
 	$.getJSON(path, function(data){ // VORSICHT, dieser Callback Handler wird nur ausgeführt, wenn das abgerufene JSON WOHLGEFORMT ist
 			console.log(data);
-			   var rolle = data.rolle;
+			  var rolle = data.rolle;
 				var spielstil = data.spielstil;
 				var spieltseit = data.spieltseit;
 				var regelsysteme = data.regelsysteme;
 				var genre = data.Lieblingsgenre;
-				var helden = data.Lieblingshelden;				
-			    $('#rolle').text(rolle);
+				var helden = data.Lieblingshelden;
+			  $('#rolle').text(rolle);
 				$('#spielstil').text(spielstil);
 				$('#spieltseit').text(spieltseit);
 				$('#regelsysteme').text(regelsysteme);
